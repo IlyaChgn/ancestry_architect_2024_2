11476347dd1bc8b965b0b17f4bedf2e15e1ca8a1
IlyaChgn <chugin.ilya2004@gmail.com>
2024-10-02 22:03:17 +0300

    Added description and scripts for tree

---
+++ db/migrations/005_family_tree.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS public.description;
+DROP TABLE IF EXISTS public.relation;
+DROP TABLE IF EXISTS public.node;
+DROP TABLE IF EXISTS public.layer;
+DROP TABLE IF EXISTS public.tree;
